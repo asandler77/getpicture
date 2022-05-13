cb530d4555606bc2f52fda7897769113662e6aad
Sandler, Alexey(as371x) <as371x@intl.att.com>
2022-05-13 21:52:07 +0300

    Avatar picker is working

---
+++ ios/EntryPoint.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import {Text, View} from 'react-native';
+
+const EntryPoint = () => {
+  return (
+    <View style={{flex: 1, justifyContent: 'center', alignItems: 'center'}}>
+      <Text>EntryPoint 1234567890</Text>
+    </View>
+  );
+};
+
+export default EntryPoint;
